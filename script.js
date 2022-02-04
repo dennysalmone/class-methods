ea76adea0d51ba6128a0a6fb03df29b42cbf2272
dennysalmone <98037857+dennysalmone@users.noreply.github.com>
2022-02-04 19:46:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,90 +1,54 @@
 class MyArray {
-    lengthArray = 0;
-    currentObj = {};
+    arrLength = 0;
+    object = {};
     push(...data){
         let args = data.flat(1);
         for (let i=0; i<args.length; i++) {
-            this.currentObj[this.lengthArray] = args[i]
-            this.lengthArray++
+            this.object[this.arrLength] = args[i]
+            this.arrLength++
         }
     };
     getByIndex(index){
-        return this.currentObj[index];
+        return this.object[index];
     };
     forEach(callback) {
-        let result = new MyArray();
-        let values = Object.values(this.currentObj)
-        for (let i=0; i<values.length; i++) {
-            result.push(callback(values[i]))
+        for (let i=0; i < this.object.length; i++) {
+            callback(this.object[i], i, this)
         }
-        this.currentObj = result.currentObj;
-        return undefined;
     }
     deleteByIndex(index){
-        for (let i=index; i<this.lengthArray; i++) {
-            this.currentObj[i] = this.currentObj[i+1]
+        for (let i=index; i<this.arrLength; i++) {
+            this.object[i] = this.object[i+1]
         }
-        this.lengthArray--
-        delete this.currentObj[this.lengthArray-1];
-        return;
+        this.arrLength--
+        delete this.object[this.arrLength-1];
     };
     filter (callback) {
         let result = new MyArray();
-        let values = Object.values(this.currentObj)
-        for (let i=0; i<values.length; i++) {
-            if(callback(values[i])) {
-                result.push(values[i])
+        for (let i=0; i<this.object.length; i++) {
+            if(callback(this.object[i], i, this)) {
+                result.object.push(callback(this.object[i], i, this))
+                result.arrLength++                
             }
         }
-        return result.currentObj;
+        return result;
     }
     map (callback) {
         let result = new MyArray();
-        let values = Object.values(this.currentObj)
-        for (let i=0; i<values.length; i++) {
-            result.push(callback(values[i]))
+        for (let i=0; i<this.object.length; i++) {
+            result.object.push(callback(this.object[i], i, this))
+            result.arrLength++
         }
-        return result.currentObj;
+        return result;
     }
     pop(){
-        if(this.lengthArray>0) {
-            this.lengthArray--
-            return delete this.currentObj[this.lengthArray];
-        } else {return undefined;}
+        if(this.arrLength>0) {
+            this.arrLength--
+            return delete this.object[this.arrLength];
+        }
     };
 }
 
-
-
-function shortWord(word) {
-    let curWord;
-    if (typeof word !== 'string') {
-        curWord = toString(word);
-    } else {
-        curWord = word;
-    }
-    if (curWord.length <=5) {
-        return false;
-    }
-    if (curWord.length >=6) {
-        return true;
-    }
-}  
-
-function makeUpperCase(string) {
-    let text;
-    if (typeof string !== 'string') {
-        text = toString(string);
-    } else {
-        text = string;
-    }
-
-    return text.toUpperCase();
-}
-
 const array = new MyArray ()
-array.push('shot', 'suck', 'long word', 'very long word', 'gg', 1222222)
-console.log(array.filter(shortWord))
-console.log(array.forEach(shortWord))
-console.log(array.map(makeUpperCase));
+array.push('shot', 'who', 'long word', 'very long word', 'Putin')
 
